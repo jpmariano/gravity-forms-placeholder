c722c4cc8dbda4c63c9c502013258c032d695d69
jmariano13 <jmariano@targetedvictory.com>
2014-08-24 07:59:16 -0400

    Ready For Initial Testing

--- gravity-forms-placeholder.php
+++ gravity-forms-placeholder.php
@@ -73,10 +73,76 @@ function GravityFormsPlaceholder_i18n_init() {
 GravityFormsPlaceholder_i18n_init();
 
 
+function my_standard_settings($position, $form_id){
+    
+        // Create settings on position 25 (right after Field Label)
+    
+    if($position == 25){ ?>
+            
+    <li class="admin_label_setting field_setting" style="display: list-item; ">
+    <label for="field_placeholder">Placeholder Text
+    
+    <!-- Tooltip to help users understand what this field does -->
+    <a href="javascript:void(0);" class="tooltip tooltip_form_field_placeholder" tooltip="&lt;h6&gt;Placeholder&lt;/h6&gt;Enter the placeholder/default text for this field.">(?)</a>
+                
+    </label>
+            
+    <input type="text" id="field_placeholder" class="fieldwidth-3" size="35" onKeyUp="SetFieldProperty('placeholder', this.value);">
+            
+    </li><?php }
+}
+    
+function my_gform_editor_js(){ ?>
+    <script>
+    //binding to the load field settings event to initialize the checkbox
+    jQuery(document).bind("gform_load_field_settings", function(event, field, form){
+    jQuery("#field_placeholder").val(field["placeholder"]);
+    });
+    </script>
+     <?php
+}
+    
+function my_gform_enqueue_scripts($form, $is_ajax=false){ ?>
+         <script>
+         jQuery(document).ready(function($) {
+              <?php
+                foreach($form['fields'] as $i=>$field){
+                        if(isset($field['placeholder']) && !empty($field['placeholder'])){?>
+                
+                $('#input_<?php echo $form['id']?>_<?php echo $field['id']?>').attr('placeholder','<?php echo $field['placeholder']?>');
+             <?php
+             }
+            }
+        ?>
+            
+         });
+         </script>
+             <?php
+}
+
+
 // Next, run the version check.
 // If it is successful, continue with initialization for this plugin
 if (GravityFormsPlaceholder_PhpVersionCheck()) {
     // Only load and run the init function if we know PHP version can parse it
-    include_once('gravity-forms-placeholder_init.php');
     GravityFormsPlaceholder_init(__FILE__);
+    
+    function GravityFormsPlaceholder_Plugin_Activate(){
+        error_log("Gravity Forms Placeholder Activated");
+    }
+    
+    register_deactivation_hook(__FILE__, "GravityFormsPlaceholder_Plugin_Activate");
+    
+     function GravityFormsPlaceholder_Plugin_deActivate(){
+        error_log("Gravity Forms Placeholder Activated");
+    }
+    
+    register_deactivation_hook(__FILE__, "GravityFormsPlaceholder_Plugin_deActivate");
+    
+    add_action('wp_loaded','my_standard_settings');
+    add_action('wp_loaded','my_gform_editor_js');
+    add_action('wp_loaded','my_gform_enqueue_scripts');
+    
 }
+
+
